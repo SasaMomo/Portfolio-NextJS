a7070509776c43b8d7384ac70ffc5f501e87d6c6
Sasa <momosasa044@gmail.com>
2024-06-10 16:17:31 -0300

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -10,7 +10,7 @@ export default function Contact() {
         <p>Email: sasa.lhcl@gmail.com</p>
         <p>whatsapp: (85) 99615-1455</p>
         <p></p>
-        <div>Curriculo-aqui</div>
+        <div></div>
       </div>
       <Footer />
     </>
