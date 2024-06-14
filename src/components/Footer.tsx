553407bb2008be20f15915cea95734ca7b65fbb5
Sasa <momosasa044@gmail.com>
2024-06-13 22:08:41 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,15 @@
+import { ArrowUpIcon, ArrowUpTrayIcon } from "@heroicons/react/24/solid";
+
 export default function Footer() {
   return (
-    <footer className="flex justify-between border-t-2 mt-4 container mx-auto">
-      <p className="m-2 text-xl self-start">All rights reserved ©</p>
-      <a href="#headerhef" className="m-2 self-end underline">
-        ↑Voltar ao topo↑
-      </a>
+    <footer className="flex flex-wrap justify-around md:justify-between border-t-2 mt-4 container mx-auto p-4">
+      <p className="m-2 text-xl self-start">All rights reserved 2024 ©</p>
+      <div className="flex">
+        <a href="#headerhef" className="m-2 underline text-xl">
+          Voltar ao topo
+        </a>
+        <ArrowUpIcon className="animate-bounce h-10 w-10 m-auto" />
+      </div>
     </footer>
   );
 }
