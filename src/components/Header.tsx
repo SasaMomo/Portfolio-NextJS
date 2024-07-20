22bcf0e3ad1ddbc3228bc127e107c85dae69d1db
Lucas <83623947+SasaMomo@users.noreply.github.com>
2024-07-19 22:20:45 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,7 +41,7 @@ export default function Header() {
           </nav>
         </div>
         <div className="flex grow items-center justify-end sm:hidden">
-          <Popover.Button className="inline-flex items-center justify-center rounded-md bg-purple-700 p-2 hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+          <Popover.Button className="inline-flex items-center justify-center rounded-md bg-purple-700 p-2 m-2 hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
             <span className="sr-only">Open menu</span>
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </Popover.Button>
